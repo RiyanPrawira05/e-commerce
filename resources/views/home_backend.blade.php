cf6bd8e474063717b502c82221e1b109c6bd2e23
riyanprawira <riyanprawira05@gmail.com>
2018-11-17 18:18:42 +0700

    template

---
+++ resources/views/home_backend.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.backend')
+
+@section('content')
+    
+    
+
+@endsection
+
+<!-- <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are logged in!
+                </div> -->
---
+++ resources/views/welcome_backend.blade.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
+    <meta name="author" content="Creative Tim">
+    <title>Argon Dashboard - Free Dashboard for Bootstrap 4</title>
+    <!-- Favicon -->
+    <link href="{{ asset('backend/img/brand/favicon.png') }}" rel="icon" type="image/png">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+    <!-- Icons -->
+    <link href="{{ asset('backend/vendor/nucleo/css/nucleo.css') }}" rel="stylesheet">
+    <link href="{{ asset('backend/vendor/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet') }}">
+    <!-- Argon CSS -->
+    <link type="text/css" href="{{ asset('backend/css/argon.css') }}" rel="stylesheet">
+</head>
+
+<body class="bg-default">
+<div class="main-content">
+    <!-- Navbar -->
+    <nav class="navbar navbar-top navbar-horizontal navbar-expand-md navbar-dark">
+        <div class="container px-4">
+            <a class="navbar-brand" href="../index.html">
+                <img src="{{ asset('backend/img/brand/white.png') }}" />
+            </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse-main" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span>
+                </button>
+
+            <div class="collapse navbar-collapse" id="navbar-collapse-main">
+            <!-- Collapse header -->
+            <div class="navbar-collapse-header d-md-none">
+                <div class="row">
+                    <div class="col-6 collapse-brand">
+                        <a href="../index.html">
+                          <img src="{{ asset('backend/img/brand/blue.png') }}">
+                        </a>
+                    </div>
+                    <div class="col-6 collapse-close">
+                        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar-collapse-main" aria-controls="sidenav-main" aria-expanded="false" aria-label="Toggle sidenav">
+                        <span></span>
+                        <span></span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Navbar items -->
+            @if (Route::has('login'))
+            <ul class="navbar-nav ml-auto">
+                @auth
+                <li class="nav-item">
+                    <a class="nav-link nav-link-icon" href="../index.html"><i class="ni ni-planet"></i><span class="nav-link-inner--text">Dashboard</span></a>
+                </li>
+                @else
+                <li class="nav-item">
+                    <a class="nav-link nav-link-icon" href="../examples/login.html"><i class="ni ni-key-25"></i><span class="nav-link-inner--text">Login</span></a>
+                </li>
+                @if (Route::has('register'))
+                <li class="nav-item">
+                    <a class="nav-link nav-link-icon" href="../examples/register.html"><i class="ni ni-circle-08"></i><span class="nav-link-inner--text">Register</span></a>
+                </li>
+                @endif
+                @endauth
+            </ul>
+            @endif
+            </div>
+        </div>
+    </nav>
+
+    <!-- Header -->
+    <div class="header bg-gradient-primary py-7 py-lg-8">
+        <div class="container">
+            <div class="header-body text-center mb-7">
+                <div class="row justify-content-center">
+                    <div class="col-lg-5 col-md-6">
+                        <h1 class="text-white">Welcome!</h1>
+                            <p class="text-lead text-light">Use these awesome forms to login or create new account in your project for free.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="separator separator-bottom separator-skew zindex-100">
+            <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
+            </svg>
+        </div>
+    </div>
+
+    <!-- Footer -->
+    <footer class="py-5">
+        <div class="container">
+            <div class="row align-items-center justify-content-xl-between">
+                <div class="col-xl-6">
+                    <div class="copyright text-center text-xl-left text-muted">
+                        &copy; 2018 <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a>
+                    </div>
+                </div>
+                <div class="col-xl-6">
+                    <ul class="nav nav-footer justify-content-center justify-content-xl-end">
+                        <li class="nav-item">
+                            <a href="javascript:0" class="nav-link" target="_blank">Creative Tim</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="javascript:0" class="nav-link" target="_blank">About Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="javascript:0" class="nav-link" target="_blank">Blog</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="javascript:0" class="nav-link" target="_blank">MIT License</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+<!-- Core -->
+<script src="{{ asset('backend/vendor/jquery/dist/jquery.min.js') }}"></script>
+<script src="{{ asset('backend/vendor/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+<!-- Argon JS -->
+<script src="{{ asset('backend/js/argon.js') }}"></script>
+</body>
+</html>
