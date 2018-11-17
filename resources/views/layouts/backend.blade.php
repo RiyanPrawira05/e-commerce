ac15d7efc59310029ba2323524411c20357b574b
riyanprawira <riyanprawira05@gmail.com>
2018-11-17 18:24:54 +0700

    template

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -247,23 +247,7 @@
 			</div>
 			<br>
 
-			<!-- Page content -->
-		   <div class="container-fluid mt--2">
-		   	<div class="row">
-		      	<div class="col-xl-4">
-		      		<div class="card bg-gradient-default shadow">
-		         		<div class="card-header bg-transparent">
-		            		<div class="row align-items-center">
-		            			<div class="col">
-				                  <h6 class="text-uppercase text-light ls-1 mb-1">Hi .. </h6>
-				                  <h2 class="text-white mb-0">WELCOME TO RD-SHOPS</h2>
-		                		</div>
-		              		</div>
-		            	</div>
-		         	</div>
-		      	</div>
-		   	</div>
-			</div>
+			@yield('content')
 
 		<!-- Core -->
 		<script src="{{ asset('backend/vendor/jquery/dist/jquery.min.js') }}"></script>
